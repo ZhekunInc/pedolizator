cb6a9678ddb3e6746701cb833078cef2a0169023
ZhekunInc <golovchak.evgen@gmail.com>
2021-08-19 16:58:38 +0300

    add files

---
+++ main/adapter.py
@@ -0,0 +1,19 @@
+from allauth.account.adapter import DefaultAccountAdapter
+
+
+class AccountAdapter(DefaultAccountAdapter):
+
+    def save_user(self, request, user, form, commit=True):
+        data = form.cleaned_data
+        user.email = data.get('email')
+        user.username = data.get('email')
+        if 'password1' in data:
+            user.set_password(data["password1"])
+        else:
+            user.set_unusable_password()
+        self.populate_username(request, user)
+        if commit:
+            # Ability not to commit makes it easier to derive from
+            # this adapter by adding
+            user.save()
+        return user
